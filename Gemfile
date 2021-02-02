0057cb1f4bdfb3e39bf9386f1fb94ef7c1a90d3f
Cynthia Tsai <tsaiyuen@gmail.com>
2021-02-02 14:55:09 -0300

    Using Factory_bot to make the tests

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'dotenv-rails'
   gem 'rspec-rails'
   gem 'ffaker'
+  gem 'factory_bot_rails'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
+    #nickname = FFaker::Name.first_name
+    #kind = %i[knight wizard].sample
+    #level = FFaker::Random.rand(100..999)
+    #user = User.new(nickname: nickname, kind: kind, level: level)
+    #Com o uso do factory_bot, pode ser trocado por:
+    user = build( :user, level: FFaker::Random.rand(100..9999))
     
     expect(user).to_not be_valid
   end
