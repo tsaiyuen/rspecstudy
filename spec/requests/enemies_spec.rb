6c41c3a6370fc59e577b739f38cd8587bb268b05
Cynthia Tsai <tsaiyuen@gmail.com>
2021-02-02 21:45:00 -0300

    Adding destroy API testing

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Enemies", type: :request do
         enemy = create(:enemy)
         enemy_attributes = attributes_for(:enemy)
         put "/enemies/#{enemy.id}", params: enemy_attributes
-
+        
         expect(enemy.reload).to have_attributes(enemy_attributes)
       end
 
@@ -46,6 +46,40 @@ RSpec.describe "Enemies", type: :request do
       end
   
     end    
+
   end
 
+  describe "DELETE /enemies" do
+    context 'when the enemy exists' do
+      it 'returns status code 204' do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+
+        expect(response).to have_http_status(204)
+      end
+
+      it 'destroy the record' do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+
+        expect { enemy.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    context "when the enemy does not exists" do
+      it 'returns status code 404' do
+        delete '/enemies/0'
+
+        expect(response).to have_http_status(404)  
+      end
+
+      it 'returns a not found message' do
+        put '/enemies/0', params: attributes_for(:enemy)
+        
+        expect(response.body).to match("Couldn't find Enemy with 'id'=0")        
+      end
+    end
+      
+      
+  end
 end
